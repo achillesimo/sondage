de1bedf3bed6795f811a1c743bd7bdf6067da501
achille@mail.com <achille@mail.com>
2017-05-07 09:03:27 +0200

    databind

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,23 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { DoughnutComponent } from './components/doughnut/doughnut.component';
+import {ChartsModule} from "ng2-charts";
+import { BarComponent } from './components/bar/bar.component';
+import { SondageComponent } from './components/sondage/sondage.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DoughnutComponent,
+    BarComponent,
+    SondageComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    ChartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
