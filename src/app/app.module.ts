807c72bb34929943abaaa25414ba903d25207583
achille@mail.com <achille@mail.com>
2017-05-07 11:34:34 +0200

    2templates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,24 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { Angular2FontawesomeModule } from 'angular2-fontawesome/angular2-fontawesome'
 
 import { AppComponent } from './app.component';
 import { DoughnutComponent } from './components/doughnut/doughnut.component';
 import {ChartsModule} from "ng2-charts";
 import { BarComponent } from './components/bar/bar.component';
 import { SondageComponent } from './components/sondage/sondage.component';
+import { UsersComponent } from './components/users/users.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DoughnutComponent,
     BarComponent,
-    SondageComponent
+    SondageComponent,
+    UsersComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
+    Angular2FontawesomeModule,
     ChartsModule
   ],
   providers: [],
